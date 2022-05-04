4089cc72209089ab455f494d9dc2f31f17eb7378
tluwaniMS <tluwani2020@gmail.com>
2022-05-04 13:14:27 +0200

    adding-router-config

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UserDisplayPageComponent } from './components/user-display-page/user-display-page.component';
+import { UserCreationPageComponent } from './components/user-creation-page/user-creation-page.component';
+import { AuthPageComponent } from './components/auth-page/auth-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'authentication', component: AuthPageComponent },
+  { path: 'user-creation', component: UserCreationPageComponent },
+  { path: 'users-display', component: UserDisplayPageComponent },
+  { path: '', redirectTo: '/authentication', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
