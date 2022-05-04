b171ea6ec0931492949a38ea7715639a0f73dead
tluwaniMS <tluwani2020@gmail.com>
2022-05-04 18:10:54 +0200

    adding-content-display-templates-for-auth

--- src/app/components/auth-page/auth-page.component.ts
+++ src/app/components/auth-page/auth-page.component.ts
@@ -3,13 +3,12 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-auth-page',
   templateUrl: './auth-page.component.html',
-  styleUrls: ['./auth-page.component.sass']
+  styleUrls: ['./auth-page.component.sass'],
 })
 export class AuthPageComponent implements OnInit {
+  signInFormDisplayIsActive = false;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
