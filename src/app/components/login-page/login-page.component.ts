94f5e7997ac10fcb22ac45018bbbf07f5d18aabd
tluwaniMS <tluwani2020@gmail.com>
2022-05-05 08:56:01 +0200

    adding-login-page-form-tem-plate

--- src/app/components/login-page/login-page.component.ts
+++ src/app/components/login-page/login-page.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
-  styleUrls: ['./login-page.component.sass']
+  styleUrls: ['./login-page.component.sass'],
 })
 export class LoginPageComponent implements OnInit {
+  constructor(private fb: FormBuilder) {}
 
-  constructor() { }
+  userLoginForm = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required]],
+  });
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
