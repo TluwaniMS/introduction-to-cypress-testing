2207d481e37cc75ba5ff3182140389a96a69edf6
tluwaniMS <tluwani2020@gmail.com>
2022-05-06 12:19:05 +0200

    adding-authentication-responses

---
+++ src/app/enumerators.ts/authentication-response-messages.ts
@@ -0,0 +1,5 @@
+export const AuthenticationResponseMessage = {
+  successful: `You've been successfuly authenticated`,
+  passwordNotMatching: `The provided credentials are incorrect`,
+  userNotMatching: `The provided user does not exist`,
+};
