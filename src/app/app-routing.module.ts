91474eb2269a5f4cd468bef5dc4e6c7b4e52d30b
tluwaniMS <tluwani2020@gmail.com>
2022-05-07 15:50:48 +0200

    adding-route-auth

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,36 @@ import { RouterModule, Routes } from '@angular/router';
 import { UserDisplayPageComponent } from './components/user-display-page/user-display-page.component';
 import { UserCreationPageComponent } from './components/user-creation-page/user-creation-page.component';
 import { AuthPageComponent } from './components/auth-page/auth-page.component';
+import {
+  AuthGuard,
+  redirectUnauthorizedTo,
+  redirectLoggedInTo,
+} from '@angular/fire/auth-guard';
+
+const redirectUnauthorisedToAuthPage = () =>
+  redirectUnauthorizedTo(['/authentication']);
+const redirectAuthorisedToUsersPage = () =>
+  redirectUnauthorizedTo(['/authentication']);
 
 const routes: Routes = [
-  { path: 'authentication', component: AuthPageComponent },
-  { path: 'user-creation', component: UserCreationPageComponent },
-  { path: 'users-display', component: UserDisplayPageComponent },
-  { path: '', redirectTo: '/authentication', pathMatch: 'full' },
+  {
+    path: 'authentication',
+    component: AuthPageComponent,
+    data: { authGuardPipe: redirectAuthorisedToUsersPage },
+  },
+  {
+    path: 'user-creation',
+    component: UserCreationPageComponent,
+    canActivate: [AuthGuard],
+    data: { authGuardPipe:redirectUnauthorisedToAuthPage },
+  },
+  {
+    path: 'users-display',
+    component: UserDisplayPageComponent,
+    canActivate: [AuthGuard],
+    data: { authGuardPipe: redirectUnauthorisedToAuthPage },
+  },
+  { path: '', redirectTo: '/users-display', pathMatch: 'full' },
 ];
 
 @NgModule({
