85180da00d3b777a1898f2ddffdcf23829449bfc
tluwaniMS <tluwani2020@gmail.com>
2022-05-08 17:05:46 +0200

    adding-super-hero-services-and-display

--- src/app/components/super-heroes-display/super-heroes-display.component.ts
+++ src/app/components/super-heroes-display/super-heroes-display.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { SuperHeroesService } from '../../services/super-heroes.service';
 
 @Component({
   selector: 'app-super-heroes-display',
   templateUrl: './super-heroes-display.component.html',
-  styleUrls: ['./super-heroes-display.component.sass']
+  styleUrls: ['./super-heroes-display.component.sass'],
 })
 export class SuperHeroesDisplayComponent implements OnInit {
+  superHeroes: any[] = [];
 
-  constructor() { }
+  constructor(private superHeroesService: SuperHeroesService) {}
 
   ngOnInit(): void {
+    this.superHeroes = this.superHeroesService.getSuperHeroes();
   }
-
 }
--- src/app/services/super-heroes.service.ts
+++ src/app/services/super-heroes.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import { SuperHeroes } from '../sample-data/super-heroes-sample-data';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class SuperHeroesService {
+  ArrayOfSuperHeroes = SuperHeroes;
+  constructor() {}
 
-  constructor() { }
+  createSuperHero(superHero: any) {
+    this.ArrayOfSuperHeroes.push(superHero);
+  }
+
+  deleteSuperHeroById(superHeroId: string) {
+    this.ArrayOfSuperHeroes = this.ArrayOfSuperHeroes.filter(
+      (superHero) => superHero._id! === superHeroId
+    );
+  }
+
+  getSuperHeroes() {
+    return this.ArrayOfSuperHeroes;
+  }
 }
