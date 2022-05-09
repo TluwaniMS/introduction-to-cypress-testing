a51a10e087e14ab9f3a59ea061dd40618a97b489
tluwaniMS <tluwani2020@gmail.com>
2022-05-09 10:52:38 +0200

    adding-the-required-logic-for-super-power-deletion

--- src/app/components/super-powers-display/super-powers-display.component.ts
+++ src/app/components/super-powers-display/super-powers-display.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import { SuperPowersService } from '../../services/super-powers.service';
+import { ContentDeletionConfirmationComponent } from '../../modals/content-deletion-confirmation/content-deletion-confirmation.component';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-super-powers-display',
   templateUrl: './super-powers-display.component.html',
-  styleUrls: ['./super-powers-display.component.sass']
+  styleUrls: ['./super-powers-display.component.sass'],
 })
 export class SuperPowersDisplayComponent implements OnInit {
-
-  constructor() { }
+  superPowers: any[] = [];
+  constructor(
+    private superPowersService: SuperPowersService,
+    public dialog: MatDialog
+  ) {}
 
   ngOnInit(): void {
+    this.superPowers = this.superPowersService.getAllSuperPowers();
   }
 
+  deleteSuperPower(superPowerId: string) {
+    this.superPowersService.deleteSuperPowerById(superPowerId);
+    this.superPowers = this.superPowersService.getAllSuperPowers();
+  }
+
+  confirmSuperPowerDeletion(superPowerId: string) {
+    const dialogRef = this.dialog.open(ContentDeletionConfirmationComponent);
+
+    dialogRef.afterClosed().subscribe((result) => {
+      result ? this.deleteSuperPower(superPowerId) : '';
+    });
+  }
 }
