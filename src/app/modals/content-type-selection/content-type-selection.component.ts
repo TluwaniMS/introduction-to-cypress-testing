e12bab43d7975fbd5ac6a96ff0cb94709cbb34e2
tluwaniMS <tluwani2020@gmail.com>
2022-05-10 10:53:19 +0200

    updating-content-type-selection-logic

--- src/app/modals/content-type-selection/content-type-selection.component.ts
+++ src/app/modals/content-type-selection/content-type-selection.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog, MatDialogRef } from '@angular/material/dialog';
+import { SuperHeroCreatorComponent } from '../super-hero-creator/super-hero-creator.component';
+import { SuperPowerCreatorComponent } from '../super-power-creator/super-power-creator.component';
 
 @Component({
   selector: 'app-content-type-selection',
   templateUrl: './content-type-selection.component.html',
-  styleUrls: ['./content-type-selection.component.sass']
+  styleUrls: ['./content-type-selection.component.sass'],
 })
 export class ContentTypeSelectionComponent implements OnInit {
+  constructor(
+    public dialog: MatDialog,
+    private matDialogRef: MatDialogRef<ContentTypeSelectionComponent>
+  ) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  contentCreationSelection(contentType: string) {
+    contentType === 'SuperHero'
+      ? this.openSuperHeroCreationModal()
+      : this.openSuperPowerCreationModal();
   }
 
+  openSuperHeroCreationModal() {
+    this.dialog.open(SuperHeroCreatorComponent);
+    this.matDialogRef.close();
+  }
+
+  openSuperPowerCreationModal() {
+    this.dialog.open(SuperPowerCreatorComponent);
+    this.matDialogRef.close();
+  }
 }
