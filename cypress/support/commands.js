dc056db1f11643986bd78fd2975083413a84591e
tluwaniMS <tluwani2020@gmail.com>
2022-06-09 11:47:37 +0200

    adding-sample-data-files-content-creation-commands

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -5,14 +5,28 @@ Cypress.Commands.add("signIn", (email, password) => {
 });
 
 Cypress.Commands.add("register", (email, password) => {
-  cy.get('.auth-prompt-container').contains('register').click()
+  cy.get(".auth-prompt-container").contains("register").click();
   cy.get(`[formControlName='email']`).type(email);
   cy.get(`[formControlName='password']`).type(password);
   cy.get(`[formControlName='passwordConfirmation']`).type(password);
   cy.get("button").contains("register").click();
 });
 
-
 Cypress.Commands.add("signOut", () => {
-  cy.get(`[matTooltip='sign out']`).click()
+  cy.get(`[matTooltip='sign out']`).click();
+});
+
+Cypress.Commands.add("createSuperHero", (name, superPowers) => {
+  cy.get(`[matTooltip='create']`).click();
+  cy.get("button").contains("Super Hero").click();
+  cy.get(`[formControlName='name']`).type(name);
+  cy.get("button").contains("create").click();
+});
+
+Cypress.Commands.add("createSuperPower", (power, description) => {
+  cy.get(`[matTooltip='create']`).click();
+  cy.get("button").contains("Super Power").click();
+  cy.get(`[formControlName='power']`).type(power);
+  cy.get(`[formControlName='description']`).type(description);
+  cy.get("button").contains("Create").click();
 });
